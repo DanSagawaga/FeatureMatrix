8cb5a111a8be8853a3e91b6482a5c696c34b41fd
DanSagawaga <saganod1@tcnj.edu>
2015-07-02 11:45:31 -0700

    July 2nd 2:45

--- MatrixProject/src/Driver.java
+++ MatrixProject/src/Driver.java
@@ -7,57 +7,36 @@ import java.util.*;
 public class Driver {
 
 	static float numOfFeatures = 0;
+	static Number weightFeature;
 	static float totalRawFeaturesInDocs = 0;
-	static float totalDocsByFeat = 0;
-	static float totalDocs= 0;
+	static Number totalDocsWithFeature = 0;
+	static Number totalDocs= 0;
 	static float idf = 0;
 	static String feature = "";
 	
 	static int tempDocID = 0;
 
-	static HashMap<Integer, Float> featIndex = new HashMap<Integer, Float>();
+	static HashMap<String, String> featIndex = new HashMap<String, String>(1000,990);
 
 
 	public static void main (String args[]) throws IOException{
 		
+		/*
+		 * PHASE 1 read 3 sequence files to get TotalDocs, TotalRawFreqs, TotalDocFreq, and index features 
+		 */
 		totalDocs = (float)getTotalDocNum("/Users/dansaganome/Desktop/2-100/class-memberships/ClassMemberships.txt");
 		System.out.println(totalDocs);
 		
-		BufferedReader featReader = readDataFile("/Users/dansaganome/Desktop/2-100/feature-sets/ne_all/docfreqs/DocFreqs.txt");
-		BufferedReader featReader2 = readDataFile("/Users/dansaganome/Desktop/2-100/feature-sets/ne_all/rawfreqs/part-00000.txt");
-
-		String curLine = "", curLine2 = "";
-		Scanner scanner; 
-		StringTokenizer tokenizer = null;
 		
-		// Reads dictionary to index features into a hash map
-		for(int k = 0; ((curLine = featReader.readLine())!= null) && ((curLine2 = featReader2.readLine())!= null); k++){
-			tokenizer = new StringTokenizer(curLine,"\t");
-			if(tokenizer.hasMoreTokens())
-			feature = tokenizer.nextToken();
-			if(tokenizer.hasMoreTokens())
-			totalDocsByFeat = Integer.parseInt(tokenizer.nextToken());
-			
-			
-			tokenizer = new StringTokenizer(curLine2,"\t");	//gets the raw number of features in all documents
-			if(tokenizer.hasMoreTokens())
-			tokenizer.nextToken();
-			if(tokenizer.hasMoreTokens())
-			totalRawFeaturesInDocs = Integer.parseInt(tokenizer.nextToken());
-			//System.out.println(k + ": " + feature + "\t" + totalDocsByFeat + "\n");
-			idf = (float) Math.log10(totalDocs / (Float.MIN_VALUE + totalDocsByFeat)); 
-			featIndex.put(k,idf);			
-		}
-		numOfFeatures = featIndex.size();
-		//System.out.println(featIndex.size());
-			
 		
 
-		featReader = readDataFile("/Users/dansaganome/Desktop/2-100/feature-sets/ne_all/docfeaturesets-weighted/docfeaturesets-weighted.xml");
+		BufferedReader featReader = readDataFile("/Users/dansaganome/Desktop/2-100/feature-sets/ne_all/docfeaturesets-weighted/docfeaturesets-weighted.xml");
 		
-		String partialXml = "";
-		String fullXml = "";
+		Scanner scanner;
+		String curLine = "", fullXml = "", partialXml = "";
 		FeatureSetWeighted XmlReader = new FeatureSetWeighted(); 
+		
+		
 		
 		for(int k = 0; ((curLine = featReader.readLine())!= null); k++){
 			scanner = new Scanner(curLine);
@@ -77,24 +56,70 @@ public class Driver {
 				}
 		//		System.out.println(fullXml);
 			}
+			XmlReader.getWeight();
+			
 		}			
 		}
 
 		
-public static int getTotalDocNum(String filePath){
-	//Reads ClassMembership seq file to get total number of Documents
-	BufferedReader featReader = readDataFile(filePath);
-	int counter = 0;
-	try{
-	while((featReader.readLine()!= null)){
-		counter++;
+	public static int getTotalDocNum(String filePath){
+		//Reads ClassMembership seq file to get total number of Documents
+		BufferedReader featReader = readDataFile(filePath);
+		int counter = 0;
+		try{
+			while((featReader.readLine()!= null)){
+				counter++;
+			}
+			return counter;
+		} catch(IOException e){
+			System.err.println("Error counting total documents: " + e.getMessage());
+			return -1;
+		}
 	}
-	return counter;
-	} catch(IOException e){
-		System.err.println("Error counting total documents: " + e.getMessage());
-		return -1;
+
+	public static void indexFeaturesWithTF(String filePath){
+		
+		BufferedReader featReader = readDataFile(filePath);
+
+		String curLine = "";
+		StringTokenizer tokenizer = null;
+		
+		try{
+		for(int k = 0; ((curLine = featReader.readLine())!= null); k++){
+			/*
+			 * Reads the Doc Frequency file first and the Doc frequency 
+			 */
+			tokenizer = new StringTokenizer(curLine,"\t");
+			if(tokenizer.hasMoreTokens())
+			feature = tokenizer.nextToken();
+			else{
+				System.out.println("DocFreq Line " + k + " : bad feature input, replaced with a \" \" instead");
+				feature = "";
+			}
+			if(tokenizer.hasMoreTokens())
+				totalDocsWithFeature = Integer.parseInt(tokenizer.nextToken());
+			else{
+				System.out.println("Line " + k + " : bad number input, replaced with a 0 instead");
+				totalDocsWithFeature = 0;
+			}
+			//System.out.println(k + ": " + feature + "\t" + totalDocsByFeat + "\n");
+			
+			/*
+			 * Computes the IDF Line values and stores it in the HashMap Table
+			 * Hash input <featureName, IDF> --k will be the feature index or column index
+			 */
+			//
+            	idf = (float) Math.log10(totalDocs.floatValue() / (Float.MIN_VALUE + totalDocsWithFeature.floatValue()));
+            	String idfWithIndex = (k + " " + idf) ;
+            	
+            	featIndex.put(feature,idfWithIndex);			
+		}
+		}catch(Exception e){
+			System.out.println("ERROR: Reading in totalDocs files: " + e.getMessage());
+		}
+		numOfFeatures = featIndex.size();
 	}
-}
+
 	
 	public static BufferedReader readDataFile(String filename) {
 		BufferedReader inputReader = null;
